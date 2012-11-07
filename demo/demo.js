732a646b8f1d140a350254e38c4c2fb88626ba3c
Mike Cantelon <mcantelon@gmail.com>
2012-11-06 17:41:35 -0800

    Did some commenting and added field visibility logic.

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,5 @@
 function demo() {
+  // create new form instance, providing a single row of default data
   var search = new advancedSearch.AdvancedSearchView({
     el: $('#search_form'),
     data: [{
@@ -8,22 +9,32 @@ function demo() {
     }]
   });
 
+  // define op field
   search.addSelect('op[]', 'boolean operator', {title: 'boolean operator'}, {
     'and': 'and',
     'or': 'or',
     'not': 'not'
   });
 
+  // define query field
   search.addInput('query[]', 'search query', {title: 'search query'});
 
+  // default field name field
   search.addSelect('field[]', 'field name', {title: 'field name'}, {
     'aipname': 'AIP name',
     'filename': 'File name',
     'uuid': 'UUID'
   });
 
+  // don't show first op field
+  search.fieldVisibilityCheck = function(rowIndex, fieldName) {
+    return rowIndex > 0 || fieldName != 'op[]';
+  };
+
+  // render HTML
   $('body').append(search.render());
 
+  // demonstrate conversion to URL params
   $('#url_conversion_button').click(function (){
     $('#url_result').text(search.toUrlParams());
   });
